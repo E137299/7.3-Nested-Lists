d6b058f70c4b6dd4ae68408a98aefa86be3b0cd6
David Yin <29613693+E137299@users.noreply.github.com>
2025-01-08 07:59:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,25 @@
 
-shopping_cart = [['tooth paste', 'q-tips', 'milk'],['milk', 'candy', 'apples'],['planner', 'pencils', 'q-tips']]
+
 def update_list(list, listIndex, itemIndex, item):
 	pass
 
-
-
-
-
-
-
-shopping_cart = [['tooth paste', 'q-tips', 'milk'],['milk', 'candy', 'apples'],['planner', 'pencils', 'q-tips']]
 def view_item(list,listIndex, itemIndex):
 	pass
 
-
-
-
-
-
-
-shopping_cart = [['tooth paste', 'q-tips', 'milk'],['milk', 'candy', 'apples'],['planner', 'pencils', 'q-tips']]
 def view_list(list, listIndex):
 	pass
 	
-
-
-shopping_cart = [['tooth paste', 'q-tips', 'milk'],['milk', 'candy', 'apples'],['planner', 'pencils', 'q-tips']]
 def all_in_one(list):
 	pass
 
-
-shopping_cart = [['tooth paste', 'q-tips', 'milk'],['milk', 'candy', 'apples'],['planner', 'pencils', 'q-tips']]
 def count_item(list, item):
 	pass
 
-shopping_cart = [['tooth paste', 'q-tips', 'milk'],['milk', 'candy', 'apples'],['planner', 'pencils', 'q-tips']]
 def drink_more_milk(list):
 	pass
-
-shopping_cart = [['tooth paste', 'q-tips', 'milk'],['milk', 'candy', 'apples'],['planner', 'pencils', 'q-tips']]
-def if_you_give_a_moose_a_cookie(list):
+	
+def add_a_cookie(list):
 	pass
 
-
-shopping_cart = [['tooth paste', 'q-tips', 'milk'],['milk', 'candy', 'apples'],['planner', 'pencils', 'q-tips']]
 def reverse_lists_and_items(list):
-	for inner in list:
-		inner = inner[::]
-	list = list[::]
-	return list
+	pass
